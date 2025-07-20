e4eacabbac6d90d4ecb8d6c40f7603c1053cbfab
Anis <me.anis.dev@gmail.com>
2025-07-20 23:51:06 +0100

    Add mapping for CIFAR-10 to ImageNet dataset

---
+++ src/utils/mappings.py
@@ -0,0 +1,15 @@
+DATASET_TO_IMAGENET = {
+    "cifar10": {
+        "airplane": 404,     # airliner
+        "automobile": 817,   # sports car
+        "bird": 16,          # robin
+        "cat": 281,          # tabby cat
+        "deer": 354,         # elk
+        "dog": 207,          # golden retriever
+        "frog": 30,          # tree frog
+        "horse": 339,        # sorrel
+        "ship": 814,         # liner
+        "truck": 569,        # pickup
+    },
+    "imagenet": "native"  # Tiny ImageNet uses valid ImageNet subset
+}
