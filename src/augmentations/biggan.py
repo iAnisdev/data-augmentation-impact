851ca626cc25a62cdefc6cb6901a8b48e19a74fd
Anis <me.anis.dev@gmail.com>
2025-07-20 15:56:06 +0100

    Refactor BigGAN augmentation to remove unused imports and streamline output processing

--- src/augmentations/biggan.py
+++ src/augmentations/biggan.py
@@ -1,7 +1,6 @@
 import torch
-from PIL import Image
-from torchvision.transforms import ToTensor, Resize, Normalize, Compose, ToPILImage
-from pytorch_pretrained_biggan import (BigGAN, one_hot_from_int, truncated_noise_sample)
+from pytorch_pretrained_biggan import BigGAN, one_hot_from_int, truncated_noise_sample
+import torch.nn.functional as F
 
 class BigGANAugmentation:
     def __init__(self, class_id=207, truncation=0.4, device="cpu"):
@@ -11,16 +10,9 @@ class BigGANAugmentation:
         self.model = BigGAN.from_pretrained("biggan-deep-256").to(device)
         self.model.eval()
 
-        self.to_pil = ToPILImage()
-        self.to_tensor = Compose([
-            Resize((64, 64)),
-            ToTensor(),
-            Normalize([0.5]*3, [0.5]*3),
-        ])
-
-    def __call__(self, _=None):
-        noise_vector = truncated_noise_sample(truncation=self.truncation, batch_size=1)
-        class_vector = one_hot_from_int([self.class_id], batch_size=1)
+    def __call__(self, batch_size=1):
+        noise_vector = truncated_noise_sample(truncation=self.truncation, batch_size=batch_size)
+        class_vector = one_hot_from_int([self.class_id] * batch_size, batch_size=batch_size)
 
         noise_vector = torch.from_numpy(noise_vector).to(self.device)
         class_vector = torch.from_numpy(class_vector).to(self.device)
@@ -28,6 +20,13 @@ class BigGANAugmentation:
         with torch.no_grad():
             output = self.model(noise_vector, class_vector, truncation=self.truncation)
 
+        # Output is in [-1, 1], rescale to [0, 1]
         output = (output + 1) / 2
-        img = output.squeeze(0).cpu()
-        return self.to_tensor(self.to_pil(img))
+
+        # Resize to 64x64 in batch using interpolation
+        resized = F.interpolate(output, size=(64, 64), mode="bilinear", align_corners=False)
+
+        # Normalize to [-1, 1]
+        normalized = (resized - 0.5) / 0.5
+
+        return normalized.cpu()  # Return as tensor batch
