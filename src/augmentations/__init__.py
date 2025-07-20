7a3d47f378914abcc89f9afb9b54a566b7769350
Anis <me.anis.dev@gmail.com>
2025-07-20 03:16:47 +0100

    Add BigGANAugmentation to the public API in __init__.py

--- src/augmentations/__init__.py
+++ src/augmentations/__init__.py
@@ -6,10 +6,12 @@ from .mixup import MixupAugmentation
 from .lsb import LSBAugmentation
 from .vqvae_augment import VQVAEAugmentation
 from .fusion import FusionAugmentation
+from .biggan import BigGANAugmentation
 
 __all__ = [
     "Augmentation",
     "AutoAugmentAugmentation",
+    "BigGANAugmentation",
     "TraditionalAugmentation",
     "MiAMixAugmentation",
     "MixupAugmentation",
