975215705b4974a2eadc1f5b3ffc8bce82ff886e
Anis <me.anis.dev@gmail.com>
2025-07-22 01:34:34 +0100

    Add init file to expose downloader functions for dataset management

---
+++ src/pipelines/load/_init__.py
@@ -0,0 +1,15 @@
+from .downloader import (
+    download_dataset,
+    download_single_dataset,
+    check_dataset_exists,
+    get_data_root,
+    SUPPORTED_DATASETS,
+)
+
+__all__ = [
+    "download_dataset",
+    "download_single_dataset",
+    "check_dataset_exists",
+    "get_data_root",
+    "SUPPORTED_DATASETS",
+]
