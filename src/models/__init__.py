a4647d4e127b5007b6b3b045b7ddf3b175322dbc
Anis <me.anis.dev@gmail.com>
2025-08-07 00:13:07 +0100

    Add VQVAE, CNN, ResNet, and EfficientNet models with their trainers and evaluators to the model initialization

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,29 @@
 from .vqvae.model import VQVAE
 from .vqvae.trainer import train_vqvae
+from .cnn.model import CNNModel
+from .cnn.trainer import train_model as train_cnn
+from .cnn.evaluator import evaluate_model as evaluate_cnn
+from .resnet.model import ResNet18, ResNet50, get_pretrained_resnet18, get_pretrained_resnet50
+from .resnet.trainer import train_model as train_resnet
+from .resnet.evaluator import evaluate_model as evaluate_resnet
+from .efficientnet.model import EfficientNetB0, get_pretrained_efficientnet_b0
+from .efficientnet.trainer import train_model as train_efficientnet
+from .efficientnet.evaluator import evaluate_model as evaluate_efficientnet
+
+__all__ = [
+    "VQVAE",
+    "train_vqvae",
+    "CNNModel",
+    "train_cnn",
+    "evaluate_cnn",
+    "ResNet18",
+    "ResNet50",
+    "get_pretrained_resnet18",
+    "get_pretrained_resnet50",
+    "train_resnet",
+    "evaluate_resnet",
+    "EfficientNetB0",
+    "get_pretrained_efficientnet_b0",
+    "train_efficientnet",
+    "evaluate_efficientnet",
+]
