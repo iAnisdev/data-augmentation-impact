f6f1de20f177991ca0b77d3d5335e8d28dd75c9a
Anis <me.anis.dev@gmail.com>
2025-08-07 00:50:20 +0100

    Add verify_datasets_ready_for_training to preprocessing module exports

--- src/pipelines/preprocess/__init__.py
+++ src/pipelines/preprocess/__init__.py
@@ -1,13 +1,14 @@
 from .main import run_preprocessing_pipeline
 from .base_preprocess import preprocess_all
 from .vqvae_pipeline import get_vqvae_model, apply_vqvae
-from .verifier import verify_all_preprocessed
+from .verifier import verify_all_preprocessed, verify_datasets_ready_for_training
 from .config import SUPPORTED_AUGMENTATIONS
 
 __all__ = [
     "SUPPORTED_AUGMENTATIONS",
     "run_preprocessing_pipeline",
     "verify_all_preprocessed",
+    "verify_datasets_ready_for_training",
     "preprocess_all",
     "get_vqvae_model",
     "apply_vqvae",
